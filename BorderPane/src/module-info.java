810161098aa4eb57ffbd3465ef30aacaf31f0245
zw <ivan492764905@gmail.com>
2019-03-16 03:17:20 -0400

    new

--- BorderPane/src/module-info.java
+++ BorderPane/src/module-info.java
@@ -1,4 +1,4 @@
-module hbox {
+module BorderPane {
     requires javafx.fxml;
     requires javafx.controls;
     opens sample;
--- JGridPane/layouts/src/module-info.java
+++ JGridPane/layouts/src/module-info.java
@@ -1,5 +1,6 @@
-module hbox {
+module layouts {
     requires javafx.fxml;
     requires javafx.controls;
+
     opens sample;
 }
